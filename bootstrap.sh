d7d77959ecc4809834c39de6f2057607add39f52
Zac Smith <ztsmith@gmail.com>
2017-07-04 15:05:56 -0700

    Simplify bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-sudo apt-get -y install \
-  python-setuptools python-dev \
-  aptitude \
-  git \
-  libxml2-dev libxslt-dev \
-  software-properties-common
-
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get -y install ansible
+sudo apt-get -y install aptitude ansible
 
 ansible-galaxy install -r requirements.yml
